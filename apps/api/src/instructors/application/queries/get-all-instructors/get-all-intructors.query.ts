1a2579feec917b0e56cb1e09e4158b4a4a06863b
joseeg.perez <joseegpc@gmail.com>
2024-05-01 15:01:12 -0400

    docs(infrastructure): :memo: documentation added

--- apps/api/src/instructors/application/queries/get-all-instructors/get-all-intructors.query.ts
+++ apps/api/src/instructors/application/queries/get-all-instructors/get-all-intructors.query.ts
@@ -4,13 +4,13 @@ import { GetAllInstructorsResponse } from './types/response.type';
 import { ApplicationService, Result } from '@app/core';
 
 export class GetAllInstructors implements ApplicationService<void, GetAllInstructorsResponse> {
-    constructor( private readonly instructorRepository: InstructorRepository ) { }
-    
+    constructor(private readonly instructorRepository: InstructorRepository) { }
+
     async execute(data: void): Promise<Result<GetAllInstructorsResponse>> {
         const instructors = await this.instructorRepository.findAllInstructors();
 
-        if(instructors.length === 0) return Result.failure(new InstructorsListEmpty());
+        if (instructors.length === 0) return Result.failure(new InstructorsListEmpty());
 
-        return Result.success<GetAllInstructorsResponse>({instructors});
+        return Result.success<GetAllInstructorsResponse>({ instructors });
     }
 }
--- apps/api/src/instructors/infraestructure/controllers/instructor.controller.ts
+++ apps/api/src/instructors/infraestructure/controllers/instructor.controller.ts
@@ -4,15 +4,27 @@ import { GetInstructorByIdDto } from '../../application/queries/get-instructor-b
 import { GetAllInstructors } from '../../application/queries/get-all-instructors/get-all-intructors.query';
 import { MongoInstructorRepository } from '../repositories/mongo-instructor.repository';
 import { Instructor as InstructorModel } from '../models/instructor.model';
+import { InstructorResponse } from '../responses/instructor.response';
 import { Controller, Get, Param } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('instructors')
+@ApiTags('Instructors')
 export class InstructorsController {
     constructor(
         @InjectModel(InstructorModel.name) private readonly model: Model<InstructorEntity>) { }
 
+    @ApiResponse({
+        status: 200,
+        description: 'The instructor has been successfully found',
+        type: [InstructorResponse],
+    })
+    @ApiResponse({
+        status: 404,
+        description: 'Instructor not found',
+    })
     @Get(':id')
     findOneInstructor(@Param('id') id: string) {
         const data: GetInstructorByIdDto = { id };
@@ -22,6 +34,11 @@ export class InstructorsController {
         return getInstructorByIdQuery.execute(data);
     }
 
+    @ApiResponse({
+        status: 200,
+        description: 'Instructors list',
+        type: [InstructorResponse],
+    })
     @Get()
     findAllInstructors() {
         const repository = new MongoInstructorRepository(this.model);

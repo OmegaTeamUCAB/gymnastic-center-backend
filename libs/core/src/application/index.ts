3a43995e0c35c530af4c4d14708219fc4f0cea57
Carlos Fornari <lilfornah@gmail.com>
2024-05-05 19:35:39 -0400

    feat(application): code generator interface

--- libs/core/src/application/index.ts
+++ libs/core/src/application/index.ts
@@ -1,5 +1,6 @@
 export { ApplicationService } from './services/application-service';
 export { ApplicationException } from './exceptions/application-exception';
+export { CodeGenerator } from './codes/code-generator.interface';
 export { EmailHandler } from './email/email-handler.interface';
 export { EventHandler } from './event-handler/event-handler';
 export { ExceptionCatcherDecorator } from './decorators/exception-catcher';

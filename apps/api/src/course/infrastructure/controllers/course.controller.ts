f5d3891453f2da2b4de731857604ec3ba20b7625
Carlos Fornari <lilfornah@gmail.com>
2024-05-05 00:24:08 -0400

    feat(infrastructure): :sparkles: course endpoints

---
+++ apps/api/src/course/infrastructure/controllers/course.controller.ts
@@ -0,0 +1,118 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Param,
+  ParseUUIDPipe,
+  Post,
+} from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { IdGenerator, IdResponse, UUIDGENERATOR } from '@app/core';
+import { COURSE_REPOSITORY } from '../constants';
+import { CourseRepository } from '../../domain';
+import { CourseResponse } from './responses/course.response';
+import {
+  CreateCourseCommand,
+  GetCourseByIdQuery,
+  GetCoursesByCategoryQuery,
+  GetCoursesByInstructorQuery,
+  GetCoursesQuery,
+  UpdateCourseCommand,
+} from '../../application';
+import { CreateCourseDto, UpdateCourseDto } from './dtos';
+
+@Controller('courses')
+@ApiTags('Courses')
+export class CourseController {
+  constructor(
+    @Inject(COURSE_REPOSITORY)
+    private readonly courseRepository: CourseRepository,
+    @Inject(UUIDGENERATOR)
+    private readonly uuidGenerator: IdGenerator<string>,
+  ) {}
+
+  @Get()
+  @ApiResponse({
+    status: 200,
+    description: 'Courses list',
+    type: [CourseResponse],
+  })
+  async getCategories() {
+    const service = new GetCoursesQuery(this.courseRepository);
+    const result = await service.execute();
+    return result.unwrap();
+  }
+
+  @Get('category/:id')
+  @ApiResponse({
+    status: 200,
+    description: 'Courses by category',
+    type: [CourseResponse],
+  })
+  async getCoursesByCategory(@Param('id', ParseUUIDPipe) categoryId: string) {
+    const service = new GetCoursesByCategoryQuery(this.courseRepository);
+    const result = await service.execute({ categoryId });
+    return result.unwrap();
+  }
+
+  @Get('instructor/:id')
+  @ApiResponse({
+    status: 200,
+    description: 'Courses by instructor',
+    type: [CourseResponse],
+  })
+  async getCoursesByInstruvtor(
+    @Param('id', ParseUUIDPipe) instructorId: string,
+  ) {
+    const service = new GetCoursesByInstructorQuery(this.courseRepository);
+    const result = await service.execute({ instructorId });
+    return result.unwrap();
+  }
+
+  @Get(':id')
+  @ApiResponse({
+    status: 200,
+    description: 'Course found',
+    type: CourseResponse,
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Course not found',
+  })
+  async getCourseById(@Param('id', ParseUUIDPipe) id: string) {
+    const service = new GetCourseByIdQuery(this.courseRepository);
+    const result = await service.execute({ id });
+    return result.unwrap();
+  }
+
+  @Post()
+  @ApiResponse({
+    status: 200,
+    description: 'Course created',
+    type: IdResponse,
+  })
+  async createCategory(@Body() createCategoryDto: CreateCourseDto) {
+    const service = new CreateCourseCommand(
+      this.courseRepository,
+      this.uuidGenerator,
+    );
+    const result = await service.execute(createCategoryDto);
+    return result.unwrap();
+  }
+
+  @Post(':id')
+  @ApiResponse({
+    status: 200,
+    description: 'Course updated',
+    type: IdResponse,
+  })
+  async updateCategory(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateCategoryDto: UpdateCourseDto,
+  ) {
+    const service = new UpdateCourseCommand(this.courseRepository);
+    const result = await service.execute({ id, ...updateCategoryDto });
+    return result.unwrap();
+  }
+}
---
+++ apps/api/src/course/infrastructure/controllers/dtos/index.ts
@@ -0,0 +1,2 @@
+export { CreateCourseDto } from './create-course.dto';
+export { UpdateCourseDto } from './update-course.dto';

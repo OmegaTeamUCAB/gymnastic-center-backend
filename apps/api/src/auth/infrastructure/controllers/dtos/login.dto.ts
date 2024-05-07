b95d7c5c01fc29febca1f189597ae221dabf341c
Carlos Fornari <lilfornah@gmail.com>
2024-05-07 14:10:06 -0400

    docs(infrastructure): :memo: swagger missing dtos

--- apps/api/src/auth/infrastructure/controllers/dtos/login.dto.ts
+++ apps/api/src/auth/infrastructure/controllers/dtos/login.dto.ts
@@ -1,10 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class LoginDto {
   @IsEmail()
+  @ApiProperty()
   email: string;
 
   @IsString()
   @MinLength(8)
+  @ApiProperty({
+    minLength: 8,
+  })
   password: string;
 }

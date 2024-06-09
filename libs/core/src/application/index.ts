26f5e052adba2477b8f8931f291df5a3a3415276
Carlos Fornari <lilfornah@gmail.com>
2024-06-09 19:37:52 -0400

    chore: add export

--- libs/core/src/application/index.ts
+++ libs/core/src/application/index.ts
@@ -3,6 +3,7 @@ export { ApplicationException } from './exceptions/application-exception';
 export { CodeGenerator } from './codes/code-generator.interface';
 export { EmailHandler } from './email/email-handler.interface';
 export { EventHandler } from './event-handler/event-handler';
+export { EventStore } from './event-store/event-store';
 export { ExceptionCatcherDecorator } from './decorators/exception-catcher';
 export { IdGenerator } from './id/id-generator.interface';
 export { CryptoService } from './crypto/crypto-service.interface';

7e0c2a140a87aeb52d8771422fbb5e96f0473086
Carlos Fornari <lilfornah@gmail.com>
2024-06-08 21:34:44 -0400

    fix(application): 🏷️ fix event store contract

--- libs/core/src/application/event-store/event-store.ts
+++ libs/core/src/application/event-store/event-store.ts
@@ -1,7 +1,7 @@
 import { DomainEvent } from '@app/core/domain';
 
 export interface EventStore {
-  append(events: DomainEvent[]): Promise<void>;
+  append(dispatcherId: string, events: DomainEvent[]): Promise<void>;
   getEventsByDispatcherId(dispatcherId: string): Promise<DomainEvent[]>;
   getEventsByDateRange(from?: Date, until?: Date): Promise<DomainEvent[]>;
 }

f840dfc9a1114ee2a536db1ed0be3cd66085ea5d
LuisElianM <luiselian001@gmail.com>
2024-06-10 19:03:35 -0400

    build(domain): :fire: user repository removed

--- apps/api/src/user/domain/repositories/user.repository.interface.ts
+++
@@ -1,9 +0,0 @@
-import { User } from '../entities';
-
-export interface UserRepository {
-  findAllUsers(): Promise<User[]>;
-  findUserById(id: string): Promise<User>;
-  findUserByEmail(email: string): Promise<User>;
-  saveUser(user: User): Promise<void>;
-  deleteUserById(id: string): Promise<void>;
-}

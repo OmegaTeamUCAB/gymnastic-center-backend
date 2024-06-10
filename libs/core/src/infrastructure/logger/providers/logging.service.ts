a5e1f2401ed918d6b9f54a702f7c45641f5a108f
Luis Diaz <luisnando710@protonmail.com>
2024-06-10 11:30:35 -0400

    refactor(infrastructure): :recycle: class name changes & unused method removed

--- libs/core/src/infrastructure/logger/providers/logging.service.ts
+++ libs/core/src/infrastructure/logger/providers/logging.service.ts
@@ -1,19 +1,15 @@
 import { Injectable, Logger } from "@nestjs/common";
-import { LoggerService } from "@app/core/application";
+import { NativeLoggerService } from "@app/core/application";
 
 @Injectable()
-export class NestLoggerService implements LoggerService {
+export class NativeLogger implements NativeLoggerService {
     private readonly logger = new Logger();
     
     log(message: string): void {
         this.logger.log(message);
     }
     
-    error(message: string): void {
+    logError(message: string): void {
         this.logger.error(message);
-    }
-    
-    warn(message: string): void {
-        this.logger.warn(message);
     }
 }

f8a93d64c119c14064c1179e292132b9edf61333
LuisElianM <luiselian001@gmail.com>
2024-06-11 01:05:36 -0400

    style(application): :art: format applied to create user command

--- apps/api/src/user/application/commands/create-user/create-user.command-handler.ts
+++ apps/api/src/user/application/commands/create-user/create-user.command-handler.ts
@@ -1,8 +1,19 @@
-import { ApplicationService, EventHandler, EventStore, Result } from '@app/core';
+import {
+  ApplicationService,
+  EventHandler,
+  EventStore,
+  Result,
+} from '@app/core';
 import { CreateUserCommand, CreateUserResponse } from './types';
 import { IdGenerator } from '@app/core/application/id/id-generator.interface';
 import { User } from '../../../domain';
-import { UserEmail, UserId, UserImage, UserName, UserPhone } from '../../../domain/value-objects';
+import {
+  UserEmail,
+  UserId,
+  UserImage,
+  UserName,
+  UserPhone,
+} from '../../../domain/value-objects';
 
 export class CreateUserCommandHandler
   implements ApplicationService<CreateUserCommand, CreateUserResponse>
@@ -10,27 +21,27 @@ export class CreateUserCommandHandler
   constructor(
     private readonly idGenerator: IdGenerator<string>,
     private readonly eventStore: EventStore,
-    private readonly eventHandler: EventHandler
+    private readonly eventHandler: EventHandler,
   ) {}
 
-  async execute(command: CreateUserCommand): 
-  Promise<Result<CreateUserResponse>> {
+  async execute(
+    command: CreateUserCommand,
+  ): Promise<Result<CreateUserResponse>> {
     const id = this.idGenerator.generateId();
-    
+
     const data = {
       name: new UserName(command.name),
       email: new UserEmail(command.email),
       phone: new UserPhone(command.phone),
       image: new UserImage(command.image),
-    }
+    };
 
     const user = User.create(new UserId(id), data);
     const events = user.pullEvents();
     await this.eventStore.appendEvents(id, events);
     this.eventHandler.publishEvents(events);
     return Result.success<CreateUserResponse>({
-      id
-    })
-
+      id,
+    });
   }
 }

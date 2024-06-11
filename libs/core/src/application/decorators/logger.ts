400410106d746ce7ad1126532aca6097f0f1db91
Luis Diaz <luisnando710@protonmail.com>
2024-06-10 20:32:19 -0400

    fix(application): :recycle: remove try-catch

---
+++ libs/core/src/application/decorators/logger.ts
@@ -0,0 +1,22 @@
+import { ApplicationService } from '../services/application-service';
+import { Result } from '../result-wrapper/result';
+import { Logger } from '../logger/logger.interface';
+
+export class LoggingDecorator<T, U>
+  implements ApplicationService<T, U> {
+
+  constructor(
+    private readonly service: ApplicationService<T, U>,
+    private readonly logger: Logger
+  ) { }
+
+  async execute(data: T): Promise<Result<U>> {
+    this.logger.log(`${JSON.stringify(data)}`);
+    const result = await this.service.execute(data);
+
+    if (result.isFailure)
+      this.logger.logError(`Execution failed: ${result.unwrap()}`);
+
+    return result;
+  }
+}

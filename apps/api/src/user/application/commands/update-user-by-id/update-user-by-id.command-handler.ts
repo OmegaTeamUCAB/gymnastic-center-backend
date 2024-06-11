9811a85a4cb37e7d8a12ee6bc8f8043181964474
LuisElianM <luiselian001@gmail.com>
2024-06-11 01:04:44 -0400

    refactor(application): :art: refactor update user by id command

---
+++ apps/api/src/user/application/commands/update-user-by-id/update-user-by-id.command-handler.ts
@@ -0,0 +1,41 @@
+import {
+  ApplicationService,
+  EventHandler,
+  EventStore,
+  Result,
+} from '@app/core';
+import { UpdateUserCommand, UpdateUserResponse } from './types';
+import { UserNotFoundException } from '../../exceptions/user-not-found.exception';
+import { User } from '../../../domain';
+import {
+  UserId,
+  UserImage,
+  UserName,
+  UserPhone,
+} from '../../../domain/value-objects';
+
+export class UpdateUserCommandHandler
+  implements ApplicationService<UpdateUserCommand, UpdateUserResponse>
+{
+  constructor(
+    private readonly eventStore: EventStore,
+    private readonly eventHandler: EventHandler,
+  ) {}
+
+  async execute(
+    command: UpdateUserCommand,
+  ): Promise<Result<UpdateUserResponse>> {
+    const events = await this.eventStore.getEventsByStream(command.id);
+    if (events.length === 0) throw new UserNotFoundException();
+    const user = User.loadFromHistroy(new UserId(command.id), events);
+    if (command.name) user.updateName(new UserName(command.name));
+    if (command.phone) user.updatePhone(new UserPhone(command.phone));
+    if (command.image) user.updateImage(new UserImage(command.image));
+    const newEvents = user.pullEvents();
+    await this.eventStore.appendEvents(command.id, newEvents);
+    this.eventHandler.publishEvents(newEvents);
+    return Result.success<UpdateUserResponse>({
+      id: command.id,
+    });
+  }
+}

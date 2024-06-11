ac5cc6fd7686336572f87813f71aeb36a8aa9d38
Carlos Fornari <lilfornah@gmail.com>
2024-06-10 23:30:34 -0400

    feat(infrastructure): :zap: blog queries

--- apps/api/src/blog/infrastructure/controllers/blog.controller.ts
+++ apps/api/src/blog/infrastructure/controllers/blog.controller.ts
@@ -8,20 +8,19 @@ import {
   ParseUUIDPipe,
   Query,
   ParseIntPipe,
+  DefaultValuePipe,
 } from '@nestjs/common';
-import { ApiResponse, ApiTags } from '@nestjs/swagger';
-import {
-  GetAllBlogsQuery,
-  GetBlogByIdQuery,
-  CreateBlogCommand,
-  UpdateBlogCommand,
-} from '../../application';
+import { ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CreateBlogCommand, UpdateBlogCommand } from '../../application';
 import { CreateBlogCommentDto, CreateBlogDto, UpdateBlogDto } from './dtos';
 import { IdGenerator, IdResponse, UUIDGENERATOR } from '@app/core';
 import { BlogLeanResponse, BlogResponse } from './responses';
 import { BlogRepository } from '../../domain';
 import { BLOG_REPOSITORY } from '../constants';
 import { Auth } from 'apps/api/src/auth/infrastructure/decorators';
+import { InjectModel } from '@nestjs/mongoose';
+import { MongoBlog } from '../models';
+import { Model } from 'mongoose';
 
 @Controller('blog')
 @ApiTags('Blogs')
@@ -32,24 +31,74 @@ export class BlogController {
     private readonly repository: BlogRepository,
     @Inject(UUIDGENERATOR)
     private readonly uuidGenerator: IdGenerator<string>,
+    @InjectModel(MongoBlog.name)
+    private readonly blogModel: Model<MongoBlog>,
   ) {}
 
+  @Get('many')
+  @ApiQuery({
+    name: 'perPage',
+    required: false,
+    description:
+      'Number of results to return for each type of search. DEFAULT = 8',
+    type: Number,
+  })
+  @ApiQuery({
+    name: 'page',
+    required: false,
+    description: 'Number of . DEFAULT = 1',
+    type: Number,
+  })
+  @ApiQuery({
+    name: 'filter',
+    required: false,
+    description: 'Blog Sorting',
+    type: String,
+    enum: ['POPULAR', 'RECENT'],
+  })
+  @ApiQuery({
+    name: 'trainer',
+    required: false,
+    description: 'Instructor id filter',
+    type: String,
+  })
+  @ApiQuery({
+    name: 'category',
+    required: false,
+    description: 'Category id filter',
+    type: String,
+  })
   @ApiResponse({
     status: 200,
     description: 'Blogs list',
     type: [BlogLeanResponse],
   })
-  @Get('many')
   async getAllBlogs(
-    @Query('page', ParseIntPipe) page: number,
-    @Query('perPage', ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+    @Query('perPage', new DefaultValuePipe(8), ParseIntPipe) perPage: number,
     @Query('filter') filter?: 'POPULAR' | 'RECENT',
     @Query('trainer') trainer?: string,
     @Query('category') category?: string,
-  ) {
-    const service = new GetAllBlogsQuery(this.repository);
-    const result = await service.execute();
-    return result.unwrap();
+  ): Promise<BlogLeanResponse[]> {
+    const blogs = await this.blogModel.find(
+      {
+        ...(trainer && { instructorId: trainer }),
+        ...(category && { categoryId: category }),
+      },
+      null,
+      {
+        skip: (page - 1) * perPage,
+        perPage,
+      },
+    );
+    return blogs.map((blog) => ({
+      id: blog.id,
+      title: blog.title,
+      image: blog.imageUrl,
+      trainer: blog.instructorId,
+      category: blog.categoryId,
+      date: blog.createdAt,
+    }));
   }
 
   @ApiResponse({
@@ -58,10 +107,23 @@ export class BlogController {
     type: BlogResponse,
   })
   @Get('one/:id')
-  async getBlogById(@Param('id', ParseUUIDPipe) id: string) {
-    const service = new GetBlogByIdQuery(this.repository);
-    const result = await service.execute({ id });
-    return result.unwrap();
+  async getBlogById(
+    @Param('id', ParseUUIDPipe) id: string,
+  ): Promise<BlogResponse> {
+    const blog = await this.blogModel.findOne({ aggregateId: id });
+    return {
+      id: blog.id,
+      title: blog.title,
+      description: blog.content,
+      images: [blog.imageUrl],
+      trainer: {
+        id: blog.instructorId,
+        name: 'El Tigre',
+      },
+      category: blog.categoryId,
+      date: blog.createdAt,
+      tags: blog.tags,
+    };
   }
 
   @ApiResponse({

f322ab7e2250244e58425a69adbe865cef0d3422
Luis Diaz <luisnando710@protonmail.com>
2024-06-12 20:57:56 -0400

    feat(infrastructure): :construction: performance-monitor constants

---
+++ libs/core/src/infrastructure/performance/index.ts
@@ -0,0 +1,3 @@
+export { PERFORMANCE_MONITOR_SERVICE } from './constants';
+export { NativePerformanceMonitor } from './providers/performance.service';
+export { PerformanceMonitorModule } from './performance.module';

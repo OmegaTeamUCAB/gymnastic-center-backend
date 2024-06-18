2dabc867cf4d92cd2096e408f45396a33367e868
Carlos Fornari <lilfornah@gmail.com>
2024-06-17 23:01:25 -0400

    fix(infrastructure): change miscached collection

--- libs/core/src/infrastructure/models/mongo-instructor.model.ts
+++ libs/core/src/infrastructure/models/mongo-instructor.model.ts
@@ -3,7 +3,7 @@ import { HydratedDocument, SchemaTypes } from 'mongoose';
 
 export type InstructorDocument = HydratedDocument<MongoInstructor>;
 
-@Schema({ collection: 'instructors', timestamps: false, versionKey: false })
+@Schema({ collection: 'trainers', timestamps: false, versionKey: false })
 export class MongoInstructor {
   readonly _id: string;
 

d720a5275aaaa3510e9c7e959609e2b623c339a6
Carlos Fornari <lilfornah@gmail.com>
2024-06-29 14:49:14 -0400

    feat(domain): :sparkles: calculate completion percentage service

---
+++ apps/api/src/course/domain/services/calculate-completion-percentage.ts
@@ -0,0 +1,24 @@
+import { DomainService } from '@app/core';
+import { CompletionPercentage } from '../entities/user-progress/value-objects';
+import { InvalidCompletionPercentageException } from '../entities/user-progress/exceptions';
+
+type CalculateCompletionPercentageInput = {
+  lastSecondWatched: number;
+  totalSeconds: number;
+  markAsCompleted: boolean;
+};
+
+export class CalculateCompletionPercentage
+  implements
+    DomainService<CalculateCompletionPercentageInput, CompletionPercentage>
+{
+  execute(data: CalculateCompletionPercentageInput): CompletionPercentage {
+    const { lastSecondWatched, totalSeconds, markAsCompleted } = data;
+    if (markAsCompleted) return CompletionPercentage.completed();
+    if (totalSeconds < lastSecondWatched)
+      throw new InvalidCompletionPercentageException();
+    if (totalSeconds <= lastSecondWatched + 3)
+      return CompletionPercentage.completed();
+    return new CompletionPercentage((lastSecondWatched * 100) / totalSeconds);
+  }
+}

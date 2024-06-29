c7106102ba1b57a1e7a8c9ba099abfdf56be2143
Carlos Fornari <lilfornah@gmail.com>
2024-06-29 15:08:50 -0400

    feat(application): :sparkles: watch lesson command

--- apps/api/src/course/application/commands/index.ts
+++ apps/api/src/course/application/commands/index.ts
@@ -1,2 +1,4 @@
 export { CreateCourseCommandHandler } from './create-course/create-course.command-handler';
 export { UpdateCourseCommandHandler } from './update-course/update-course.command-handler';
+export { StartCourseCommandHandler } from './start-course/start-course.command-handler';
+export { WatchLessonCommandHandler } from './watch-lesson/watch-lesson.command-handler';
--- apps/api/src/course/application/commands/watch-lesson/watch-lesson.command-handler.ts
+++ apps/api/src/course/application/commands/watch-lesson/watch-lesson.command-handler.ts
@@ -5,10 +5,8 @@ import { Course } from '../../../domain';
 import { CourseId } from '../../../domain/value-objects';
 import { UserId } from 'apps/api/src/user/domain/value-objects';
 import { LessonId } from '../../../domain/entities/lessons/value-objects';
-import {
-  CompletionPercentage,
-  LastSecondWatched,
-} from '../../../domain/entities/user-progress/value-objects';
+import { LastSecondWatched } from '../../../domain/entities/user-progress/value-objects';
+import { CalculateCompletionPercentage } from '../../../domain/services/calculate-completion-percentage';
 
 export class WatchLessonCommandHandler
   implements Service<WatchLessonCommand, WatchLessonResponse>
@@ -18,19 +16,28 @@ export class WatchLessonCommandHandler
   async execute(
     command: WatchLessonCommand,
   ): Promise<Result<WatchLessonResponse>> {
-    const events = await this.eventStore.getEventsByStream(command.courseId);
+    const {
+      courseId,
+      lastSecondWatched,
+      lessonId,
+      markAsCompleted,
+      totalSeconds,
+      userId,
+    } = command;
+    const events = await this.eventStore.getEventsByStream(courseId);
     if (events.length === 0) throw new CourseNotFoundException();
-    const course = Course.loadFromHistory(
-      new CourseId(command.courseId),
-      events,
-    );
+    const course = Course.loadFromHistory(new CourseId(courseId), events);
+    const calculateCompletionPercentageService =
+      new CalculateCompletionPercentage();
     course.watchLesson(
-      new LessonId(command.lessonId),
-      new UserId(command.userId),
-      new LastSecondWatched(command.lastSecondWatched),
-      new CompletionPercentage(
-        (command.lastSecondWatched * 100) / command.totalSeconds,
-      ),
+      new LessonId(lessonId),
+      new UserId(userId),
+      new LastSecondWatched(lastSecondWatched),
+      calculateCompletionPercentageService.execute({
+        lastSecondWatched,
+        totalSeconds,
+        markAsCompleted,
+      }),
     );
     const newEvents = course.pullEvents();
     await this.eventStore.appendEvents(course.id.value, newEvents);

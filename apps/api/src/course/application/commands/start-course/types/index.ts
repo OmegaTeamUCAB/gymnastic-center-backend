5edd57921a6bb5eb2da1fad2e93d40eba16dce56
Carlos Fornari <lilfornah@gmail.com>
2024-06-29 14:21:59 -0400

    feat(application): :sparkles: start course command

---
+++ apps/api/src/course/application/commands/start-course/types/index.ts
@@ -0,0 +1,2 @@
+export { StartCourseCommand } from './command.type';
+export { StartCourseResponse } from './response.type';
---
+++ apps/api/src/course/application/commands/start-course/types/response.type.ts
@@ -0,0 +1,3 @@
+export type StartCourseResponse = {
+  courseId: string;
+};

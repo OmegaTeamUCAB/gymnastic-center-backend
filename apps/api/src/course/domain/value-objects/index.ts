e7a52d54b785aae58d7a8f0b531cfe81292dd4a6
Carlos Fornari <lilfornah@gmail.com>
2024-06-29 20:08:20 -0400

    fix(domain): :bug: import fix

--- apps/api/src/course/domain/value-objects/index.ts
+++ apps/api/src/course/domain/value-objects/index.ts
@@ -3,8 +3,6 @@ export { CourseName } from './course-name';
 export { CourseDescription } from './course-description';
 export { CourseLevel } from './course-level';
 export { CourseTag } from './course-tag';
-export { CourseWeek } from './course-week';
-export { CourseMinute } from './course-minute';
 export { CourseImage } from './course-image';
 export { CourseDuration } from './course-duration';
 export { CoursePublishDate } from './course-publish-date';

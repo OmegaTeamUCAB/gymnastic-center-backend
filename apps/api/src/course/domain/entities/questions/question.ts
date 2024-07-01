b89bc1a12fc7e9fba56069c25c83887cff16b75c
LuisElianM <luiselian001@gmail.com>
2024-07-01 00:13:19 -0400

    style(domain): :art: Code formated

--- apps/api/src/course/domain/entities/questions/question.ts
+++ apps/api/src/course/domain/entities/questions/question.ts
@@ -1,37 +1,36 @@
-import { Entity } from "@app/core";
-import { QuestionContent, QuestionDate, QuestionId } from "./value-objects";
-import { UserId } from "apps/api/src/user/domain/value-objects";
-import { LessonId } from "../lessons/value-objects";
+import { Entity } from '@app/core';
+import { QuestionContent, QuestionDate, QuestionId } from './value-objects';
+import { UserId } from 'apps/api/src/user/domain/value-objects';
+import { LessonId } from '../lessons/value-objects';
 
 export class Question extends Entity<QuestionId> {
-    constructor(
-        id: QuestionId,
-        protected _user: UserId,
-        protected _lesson: LessonId,
-        protected _content: QuestionContent,
-        protected _date: QuestionDate,
-    ) {
-        super(id);
-    }
+  constructor(
+    id: QuestionId,
+    protected _user: UserId,
+    protected _lesson: LessonId,
+    protected _content: QuestionContent,
+    protected _date: QuestionDate,
+  ) {
+    super(id);
+  }
 
-    get user() {
-        return this._user;
-    }
+  get user() {
+    return this._user;
+  }
 
-    get lesson() {
-        return this._lesson;
-    }
+  get lesson() {
+    return this._lesson;
+  }
 
-    get content() {
-        return this._content;
-    }
+  get content() {
+    return this._content;
+  }
 
-    get date() {
-        return this._date;
-    }
+  get date() {
+    return this._date;
+  }
 
-    set content(content: QuestionContent) {
-        this._content = content;
-    }
-
+  set content(content: QuestionContent) {
+    this._content = content;
+  }
 }

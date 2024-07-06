21aee665748b626226a9077f540596df5a1c23af
joseeg.perez <joseegpc@gmail.com>
2024-07-06 05:31:42 -0400

    refactor(infrastructure): :recycle: category queries

---
+++ apps/api/src/category/infrastructure/queries/get-category-by-id.query.ts
@@ -0,0 +1,30 @@
+import { MongoCategory, MongoCourse } from '@app/core';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CategoryResponse } from '../controllers/responses';
+import { NotFoundException } from '@nestjs/common';
+import { CategoryNotFoundException } from '../../application/exceptions';
+
+type Dto = {
+  id: string;
+};
+
+export class GetCategoryByIdQuery {
+  constructor(
+    @InjectModel(MongoCourse.name)
+    private readonly categoryModel: Model<MongoCategory>,
+  ) {}
+
+  async execute(dto: Dto): Promise<CategoryResponse> {
+    const { id } = dto;
+    const category = await this.categoryModel.findOne({
+      id,
+    });
+    if (!category) throw new NotFoundException(new CategoryNotFoundException());
+    return {
+      id: category.id,
+      name: category.name,
+      icon: category.icon,
+    };
+  }
+}

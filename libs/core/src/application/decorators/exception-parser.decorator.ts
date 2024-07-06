8720cd2f3a6369e3e3c34b7e7a6dca680f5804f2
Carlos Fornari <lilfornah@gmail.com>
2024-07-06 14:07:56 -0400

    refactor: :goal_net: improve exception handling

--- libs/core/src/application/decorators/exception-parser.decorator.ts
+++ libs/core/src/application/decorators/exception-parser.decorator.ts
@@ -4,14 +4,16 @@ import { Result } from '@app/core/utils';
 export class ExceptionParserDecorator<T, U> implements Service<T, U> {
   constructor(
     private readonly service: Service<T, U>,
-    private readonly errorHandler: (error: Result<U>) => void,
+    private readonly exceptionHandler: (error: Error) => void,
   ) {}
 
   async execute(data: T): Promise<Result<U>> {
-    const result = await this.service.execute(data);
-    if (result.isFailure) {
-      this.errorHandler(result);
+    try {
+      const result = await this.service.execute(data);
+      if (result.isFailure) result.unwrap();
+      return result;
+    } catch (error) {
+      this.exceptionHandler(error);
     }
-    return result;
   }
 }

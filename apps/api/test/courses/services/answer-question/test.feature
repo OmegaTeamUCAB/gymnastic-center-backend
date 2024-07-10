6416c68f7db5fd5904efb8cbcf23ae91ff58364e
Carlos Fornari <lilfornah@gmail.com>
2024-07-10 10:51:48 -0400

    test: :test_tube:

--- apps/api/test/courses/services/answer-question/test.feature
+++ apps/api/test/courses/services/answer-question/test.feature
@@ -14,3 +14,11 @@ Feature: Answer Question
         When the instructor with id "9b3dcbae-6c98-42e2-9ff3-adfc6bb99bc2" answer the question
         Then The answer should not be created in the lesson
         But The instructor cannot answer
+
+    Scenario: The instructor that published a course, tries to answer a question twice in the course
+        Given the instructor with id "863a830b-3411-42df-8468-281e307086e9" created a course with a lesson with id "f628b755-717b-4799-960a-3fe5d1d8ee5f"
+        And an user create a question in the lesson with id "f628b755-717b-4799-960a-3fe5d1d8ee5f"
+        And the instructor with id "863a830b-3411-42df-8468-281e307086e9" answered the question
+        When the instructor with id "863a830b-3411-42df-8468-281e307086e9" answer the question
+        Then The answer should be created in the lesson
+        But The second answer should not be created in the lesson
--- apps/api/test/courses/services/answer-question/then.steps.ts
+++ apps/api/test/courses/services/answer-question/then.steps.ts
@@ -1,18 +1,17 @@
 import { Result } from '@app/core';
 import { Then, world } from '@cucumber/cucumber';
+import assert from 'assert';
 import { CreateAnswerResponse } from 'apps/api/src/course/application/commands/create-answer/types';
 import { QuestionAnswered } from 'apps/api/src/course/domain/events';
 import { EventStoreMock } from 'apps/api/test/mocks/event-store.mock';
 import { partialCompare } from 'apps/api/test/utils/partial-object-compare';
 import { InvalidInstructorToAnswerException } from 'apps/api/src/course/domain/exceptions/invalid-instructor-to-answer.exception';
-import assert from 'assert';
+import { QuestionAlreadyAnsweredException } from 'apps/api/src/course/domain/exceptions';
 
 Then('The answer should be created in the lesson', async () => {
   const courseId: string = world.courseId;
   const questionId: string = world.questionId;
   const eventStore: EventStoreMock = world.eventStore;
-  const result: Result<CreateAnswerResponse> = world.result;
-  assert.strictEqual(result.isSuccessful, true);
   const events = await eventStore.getEventsByStream(courseId);
   assert.strictEqual(events.length, 3);
   assert.strictEqual(events[2].name, QuestionAnswered.name);
@@ -36,4 +35,10 @@ Then('The instructor cannot answer', async () => {
   const result: Result<CreateAnswerResponse> = world.result;
   assert.strictEqual(result.isFailure, true);
   assert.throws(() => result.unwrap(), InvalidInstructorToAnswerException);
+});
+
+Then('The second answer should not be created in the lesson', async () => {
+  const result: Result<CreateAnswerResponse> = world.result;
+  assert.strictEqual(result.isFailure, true);
+  assert.throws(() => result.unwrap(), QuestionAlreadyAnsweredException);
 });
